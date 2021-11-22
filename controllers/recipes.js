d95b0f465ce3104fc8ff5e306e34f4c8026a75b7
d-burger <dominique.burger.de@gmai.com>
2021-11-23 00:02:07 +0100

    API version 1.0

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,3 +1,26 @@
+// ----------------- MODULES -----------------
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+// -------------------------------------------
+
 export const getAllRecipes = (req, res) => {
-  res.json("HELLO WORLD");
+  // CONNECTING TO DATABASE
+  const conString =
+    "postgres://nqglketf:J0ImdPqWToZNjcySnRQiXPEGc1cE07xu@tai.db.elephantsql.com/nqglketf"; //Can be found in the Details page
+  const client = new pg.Client(conString);
+  client.connect(function (err) {
+    if (err) {
+      return console.error("could not connect to postgres", err);
+    }
+    client.query(`SELECT * FROM recipes;`, function (err, result) {
+      if (err) {
+        console.log("Something went terribly wrong!!!");
+        client.end();
+      } else {
+        res.json(result.rows);
+        client.end();
+      }
+    });
+  });
 };
