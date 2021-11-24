bbd30c788897f5a122748d411f13c0d17a38391f
Ivis Orea <ivisorea@gmail.com>
2021-11-24 21:22:28 +0100

    importing CORS

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // ----------------- MODULES -----------------
 import express from "express";
+import cors from 'cors';
 import recipesRouter from "./routes/recipesRouter.js";
+
 // -------------------------------------------
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/recipes", recipesRouter);
 
