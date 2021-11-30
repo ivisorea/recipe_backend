f777e2caf79b43476d055946b65219a79fed72c5
d-burger <dominique.burger.de@gmai.com>
2021-11-30 16:08:51 +0100

    Version 2.0

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -10,3 +10,15 @@ export const getAllRecipes = asyncHandler(async (req, res, next) => {
   const { rowCount, rows } = await db.query("SELECT * FROM recipes;");
   res.json({ total: rowCount, recipes: rows });
 });
+
+// SINGLE RECIPE
+export const getSingleRecipe = asyncHandler(async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  const { rowCount, rows } = await db.query(
+    `SELECT * FROM recipes WHERE recipe_id=${id};`
+  );
+  if (!rowCount) throw new Error(`Recipe with ID = ${id} not found.`);
+  res.json(rows);
+});
